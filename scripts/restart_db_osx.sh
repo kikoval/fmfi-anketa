41a4bd2c6c0650b378a34c28f70990aac51d6e5d
Martin Sucha <anty.sk@gmail.com>
2011-05-11 18:02:18 +0000

    Fixnute db skripty Review URL: http://codereview.appspot.com/4530046
    git-svn-id: https://fmfi-anketa.googlecode.com/svn/trunk@107 ddd0765d-4b77-61cf-2e50-c1a9a1d2991f

--- scripts/restart_db_osx.sh
+++
@@ -1,6 +0,0 @@
-php app/console doctrine:database:drop --force
-php app/console doctrine:database:create
-chmod 777 db/anketa.sqlite
-php app/console doctrine:schema:create
-php app/console doctrine:data:load
-sudo -u _www php app/console anketa:import-otazky other/anketa.yml
