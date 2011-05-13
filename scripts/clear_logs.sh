35577aeacec3e17dc10ea7511c615553fac302be
Ivan Trančík <descent89@gmail.com>
2011-05-13 06:58:59 +0000

    dofixovany init_all a clear logs. Odporucam spustat ako sudo sh scripts/init_all.sh
    git-svn-id: https://fmfi-anketa.googlecode.com/svn/trunk@119 ddd0765d-4b77-61cf-2e50-c1a9a1d2991f

--- scripts/clear_logs.sh
+++ scripts/clear_logs.sh
@@ -5,5 +5,3 @@ LOGS=`dirname "$0"`/../app/logs
 chmod 777 "$LOGS"
 echo "Clearing logs at $LOGS "
 rm -rf "$LOGS"/*
-touch "$LOGS/dev.log"
-chmod 777 "$LOGS/dev.log"
--- scripts/init_all.sh
+++ scripts/init_all.sh
@@ -2,3 +2,5 @@ sh "`dirname "$0"`"/clear_cache.sh
 sh "`dirname "$0"`"/clear_logs.sh
 sh "`dirname "$0"`"/restart_db.sh
 sh "`dirname "$0"`"/install_assets.sh
+sh "`dirname "$0"`"/clear_cache.sh
+sh "`dirname "$0"`"/clear_logs.sh
