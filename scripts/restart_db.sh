a0b21dd564d985c401dc3be10b9e2037946c326d
Ivan Trančík <descent89@gmail.com>
2011-05-15 10:31:41 +0000

    priprava na decoupling produkcneho a develoerskeho environment Review URL: http://codereview.appspot.com/4515084
    git-svn-id: https://fmfi-anketa.googlecode.com/svn/trunk@139 ddd0765d-4b77-61cf-2e50-c1a9a1d2991f

--- scripts/restart_db.sh
+++ scripts/restart_db.sh
@@ -2,14 +2,15 @@ set -x
 SCRIPT_PATH=`dirname "$0"`
 
 CONFIG_PATH="$SCRIPT_PATH"/../app/config/config.yml
-BACKEND=`grep "default_connection:" "$CONFIG_PATH" | grep -Eo '[a-z]+$'`
+PARAMETERS_PATH="$SCRIPT_PATH"/../app/config/parameters.ini
+BACKEND=`grep "db_backend" "$PARAMETERS_PATH" | grep -Eo '=.*$' | cut -c2-`
 
-SQLITE_DATABASE_FOLDER="$SCRIPT_PATH"/../db
-SQLITE_DATABASE_FILE="$SQLITE_DATABASE_FOLDER"/anketa.sqlite
+SQLITE_DATABASE_FOLDER_PATH="$SCRIPT_PATH"/../db
+SQLITE_DATABASE_FILE_PATH="$SQLITE_DATABASE_FOLDER_PATH"/"`grep "db_sqlite_file=" "$PARAMETERS_PATH" | grep -Eo '=.*$' | cut -c2-`"
 
-MYSQL_DATABASE_NAME="anketa"
-MYSQL_LOGIN="anketa"
-MYSQL_PASS="beeliyaebNeShoot"
+MYSQL_DATABASE_NAME=`grep "db_mysql_name=" "$PARAMETERS_PATH" | grep -Eo '=.*$' | cut -c2-`
+MYSQL_USER=`grep "db_mysql_user=" "$PARAMETERS_PATH" | grep -Eo '=.*$' | cut -c2-`
+MYSQL_PASS=`grep "db_mysql_password=" "$PARAMETERS_PATH" | grep -Eo '=.*$' | cut -c2-`
 MYSQL_CLIENT="unknown"
 # check if client exists and set
 if test -n "`which "mysql"`"; then
@@ -24,7 +25,7 @@ TEACHERS_SUBJECTS_IMPORT_FILE="$SCRIPT_PATH"/../other/teachers_subjects.sql
 QUESTIONS_IMPORT_FILE="$SCRIPT_PATH"/../other/anketa.yml
 
 if test "$BACKEND" = "sqlite"; then
-chmod 777 "$SQLITE_DATABASE_FOLDER"
+chmod 777 "$SQLITE_DATABASE_FOLDER_PATH"
 fi
 
 $CONSOLE doctrine:database:drop --force
@@ -32,7 +33,7 @@ $CONSOLE doctrine:database:drop --force
 $CONSOLE doctrine:database:create
 
 if test "$BACKEND" = "sqlite"; then
-chmod 777 "$SQLITE_DATABASE_FILE"
+chmod 777 "$SQLITE_DATABASE_FILE_PATH"
 fi
 
 $CONSOLE doctrine:schema:create
@@ -42,7 +43,7 @@ $CONSOLE doctrine:data:load
 $CONSOLE anketa:import-otazky $QUESTIONS_IMPORT_FILE
 
 if test "$BACKEND" = "mysql"; then
-$MYSQL_CLIENT  -u"$MYSQL_LOGIN" -p"$MYSQL_PASS" "$MYSQL_DATABASE_NAME" < "$TEACHERS_SUBJECTS_IMPORT_FILE"
+$MYSQL_CLIENT  -u"$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DATABASE_NAME" < "$TEACHERS_SUBJECTS_IMPORT_FILE"
 elif test "$BACKEND" = "sqlite"; then
-sqlite3 "$SQLITE_DATABASE_FILE" < "$TEACHERS_SUBJECTS_IMPORT_FILE"
+sqlite3 "$SQLITE_DATABASE_FILE_PATH" < "$TEACHERS_SUBJECTS_IMPORT_FILE"
 fi
