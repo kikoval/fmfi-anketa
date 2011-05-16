6d7bb9e44816808aa7d60b3beee7a9b7e99290f6
Tomi Belan <tomi.belan@gmail.com>
2011-05-16 10:19:53 +0000

    onbeforeunload kontrola neulozenych zmien Review URL: http://codereview.appspot.com/4538061
    git-svn-id: https://fmfi-anketa.googlecode.com/svn/trunk@151 ddd0765d-4b77-61cf-2e50-c1a9a1d2991f

---
+++ src/AnketaBundle/Resources/public/js/unload.js
@@ -0,0 +1,37 @@
+
+/**
+ * @copyright Copyright (c) 2011 The FMFI Anketa authors (see AUTHORS).
+ * Use of this source code is governed by a license that can be
+ * found in the LICENSE file in the project root directory.
+ *
+ * @author     Tomi Belan <tomi.belan@gmail.com>
+ */
+
+jQuery(document).ready(function ($) {
+  function getState() {
+    var result = [];
+    $('input, textarea, option').each(function () {
+      result.push($(this).val());
+      result.push(this.checked);
+      result.push(this.selected);
+    });
+    return result.join("|");
+  }
+  var initialState = getState();
+
+  var submitting = false;
+  $('form').bind('submit', function () { submitting = true; });
+
+  window.onbeforeunload = function (event) {
+    event = event || window.event;
+
+    if (submitting) return;
+    if (getState() == initialState) return;
+
+    var question = 'V niektorých odpovediach sú neuložené zmeny! Ak nestlačíte tlačidlo "Ulož", budú stratené. Naozaj chcete odísť?';
+
+    if (event) event.returnValue = question;
+    return question;
+  };
+});
+
