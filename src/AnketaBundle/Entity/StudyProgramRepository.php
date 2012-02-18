50e6dbcfdf2521d5c393b528f84125e16574b5fb
Martin Kralik <majak47@gmail.com>
2012-02-18 14:49:28 +0100

    Optimalizacia query (group by -> distinct).

--- src/AnketaBundle/Entity/StudyProgramRepository.php
+++ src/AnketaBundle/Entity/StudyProgramRepository.php
@@ -43,15 +43,13 @@ class StudyProgramRepository extends EntityRepository {
 
     public function getAllWithAnswers($season) {
         $em = $this->getEntityManager();
-        $query = $em->createQuery("SELECT sp
+        $query = $em->createQuery("SELECT DISTINCT sp
                            FROM AnketaBundle\\Entity\\StudyProgram sp,
                            AnketaBundle\\Entity\\Answer a
                            WHERE sp.id = a.studyProgram
                            AND a.teacher IS NULL
                            AND a.subject IS NULL
                            AND a.season = :season
-                           GROUP BY sp.id
-                           HAVING COUNT(a) > 0
                            ORDER BY sp.code ASC");
         $query->setParameter('season', $season);
         return $query->getResult();
