38034671c98369da5539901bb6fcdebf6d542e15
Martin Sucha <anty.sk@gmail.com>
2012-07-10 23:58:50 +0200

    Command na migrovanie slugov.
    Este nie je uplne dokonaly, ale takto to v podstate spravi co ma.

--- src/AnketaBundle/Command/MigrateSubjectIdentificationCommand.php
+++ src/AnketaBundle/Command/MigrateSubjectIdentificationCommand.php
@@ -11,12 +11,14 @@
 
 namespace AnketaBundle\Command;
 
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use AnketaBundle\Lib\NativeCSVTableReader;
+use AnketaBundle\Lib\ConcatTableReader;
 use AnketaBundle\Lib\TableColumnResolver;
 use AnketaBundle\Lib\OldPriFSubjectIdentification;
 use AnketaBundle\Lib\SubjectIdentification;
@@ -27,7 +29,7 @@ use AnketaBundle\Lib\SubjectIdentification;
  * @package    Anketa
  * @author     Martin Sucha <anty.sk+svt@gmail.com>
  */
-class MigrateSubjectIdentificationCommand extends AbstractImportCommand {
+class MigrateSubjectIdentificationCommand extends ContainerAwareCommand {
 
     protected function configure() {
         parent::configure();
@@ -35,6 +37,7 @@ class MigrateSubjectIdentificationCommand extends AbstractImportCommand {
         $this
                 ->setName('anketa:migrate:subject-identification')
                 ->setDescription('Zmigruj identifikaciu podla textaku')
+                ->addArgument('file', InputArgument::IS_ARRAY | InputArgument::REQUIRED)
         ;
     }
 
@@ -50,15 +53,26 @@ class MigrateSubjectIdentificationCommand extends AbstractImportCommand {
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
 
-        $file = $this->openFile($input);
-
-        // nacitaj prve riadky, ktore nas nezaujimaju
-        for ($i = 0;$i < 5; $i++) {
-            fgets($file);
+        $filenames = $input->getArgument('file');
+        
+        $readers = array();
+        foreach ($filenames as $filename) {
+            $file = fopen($filename, "r");
+            if ($file === false) {
+                throw new Exception('Failed to open file: ' . $filename);
+            }
+            
+            // nacitaj prve riadky, ktore nas nezaujimaju
+            for ($i = 0;$i < 9; $i++) {
+                fgets($file);
+            }
+            $readers[] = new NativeCSVTableReader($file);
         }
-        $tableReader = new NativeCSVTableReader($file);
+
+        $tableReader = new ConcatTableReader($readers);
+        
         $tableResolver = new TableColumnResolver($tableReader);
-        $tableResolver->mapColumnByTitle('Skratka', 'code');
+        $tableResolver->mapColumnByTitle('Plná skratka', 'code');
         $tableResolver->mapColumnByTitle('Názov', 'name');
         
         $oldSubjectIdentification = new OldPriFSubjectIdentification();
@@ -67,59 +81,102 @@ class MigrateSubjectIdentificationCommand extends AbstractImportCommand {
         $conn = $this->getContainer()->get('database_connection');
 
         $conn->beginTransaction();
-
-        //$insertDepartment = $conn->prepare("
-        //            INSERT INTO Department (code, name, homepage) 
-        //            VALUES (:code, :name, :homepage)");
         
-        $selectSubject = $conn->prepare('SELECT count(id) FROM Subject
-            WHERE slug = :slug');
+        $insertUpdate = $conn->prepare('
+            INSERT INTO update_subjects VALUES (:old_code, :old_slug, :new_code, :new_slug)');
+
+        $updateSubject = $conn->prepare("
+                    UPDATE Subject SET slug=:new_slug, code=:new_code WHERE slug=:old_slug");
+        
+        $selectSubject = $conn->prepare('SELECT count(id) AS pocet FROM Subject
+            WHERE slug = :slug AND code = :code');
 
         try {
             $slugs = array();
+            $subjects = array();
             
             while (($row = $tableResolver->readRow()) !== false) {
                 $code = $row['code'];
                 $name = $row['name'];
                 
+                $subjects[$code] = $name;
+            }
+            
+            $notInDB = 0;
+            $inDB = 0;
+            
+            $updates = array();
+            
+            foreach ($subjects as $code => $name) {
+                $output->writeln($code);
                 $oldProps = $oldSubjectIdentification->identify($code, $name);
                 $newProps = $newSubjectIdentification->identify($code, $name);
                 
                 $selectSubject->bindValue('slug', $oldProps['slug']);
+                $selectSubject->bindValue('code', $oldProps['code']);
                 $selectSubject->execute();
                 $rs = $selectSubject->fetchAll(\PDO::FETCH_ASSOC);
-                $output->writeln($rs[0]);
                 
-                $output->writeln($oldProps['code'] . '->' . $newProps['code']);
-                $output->writeln($oldProps['slug'] . '->' . $newProps['slug']);
-                $output->writeln('');
+                if ($rs[0]['pocet'] == 1) {
+                    $inDB++;
+                }
+                else {
+                    $notInDB++;
+                }
+                
+                $updates[] = array('old_slug' => $oldProps['slug'],
+                    'old_code' => $oldProps['code'],
+                    'new_slug' => $newProps['slug'],
+                    'new_code' => $newProps['code']);
                 
                 $slugs[$oldProps['slug']] = true;
-                
-//                $insertDepartment->bindValue('code', $code);
-//                $insertDepartment->bindValue('name', $name);
-//                $insertDepartment->bindValue('homepage', $homepage);
-//                $insertDepartment->execute();
-                
+                                
             }
             
-            $output->writeln('--');
-            $sql = 'SELECT id, slug, name, code FROM Subject WHERE slug NOT IN (';
+            $sql = 'SELECT s.id, s.slug, s.name, s.code, COUNT(a.id) as pocet FROM Subject s LEFT JOIN Answer a ON a.subject_id = s.id WHERE s.slug NOT IN (';
             foreach (array_keys($slugs) as $index => $slug) {
                 if ($index > 0) $sql .= ', ';
                 $sql .= $conn->quote($slug);
             }
-            $sql .= ')';
+            $sql .= ') GROUP BY s.id, s.slug, s.name, s.code';
             $select = $conn->prepare($sql);
             $select->execute();
             $bad = $select->fetchAll(\PDO::FETCH_ASSOC);
-            $count = 0;
+            $notInExport = 0;
+            $noAnswers = 0;
             foreach ($bad as $row) {
-                $output->writeln($row['id']. ' ' . $row['slug']. ' ' . $row['name'] . ' ' . $row['code']);
-                $count++;
+                $output->writeln($row['id']. "\t" . $row['pocet'] . "\t" . $row['slug'].
+                        "\t" . $row['name'] . "\t" . $row['code'] );
+                $notInExport++;
+                if ($row['pocet'] == 0) {
+                    $noAnswers++;
+                }
             }
-            $output->writeln('--');
-            $output->writeln($count);
+            
+            $output->writeln('------');
+            
+            $updated = 0;
+            
+            foreach ($updates as $update) {
+                
+                $updateSubject->bindValue('old_slug', $update['old_slug']);
+                $updateSubject->bindValue('new_slug', $update['new_slug']);
+                $updateSubject->bindValue('new_code', $update['new_code']);
+                $updateSubject->execute();
+                $updated += $updateSubject->rowCount();
+                if ($updateSubject->rowCount() == 0) {
+                    $output->writeln($update['old_slug'] . "\t" . $update['old_code'] . "\t" .
+                        $update['new_slug'] . "\t" . $update['new_code']);
+                }
+            }
+            
+            $output->writeln('------');
+            $output->writeln('Mame v DB, ale nenasli sa v exporte: '.$notInExport . '(' . $noAnswers . ' bez odpovedi)');
+            $output->writeln('Mame v exporte, ale nenasli sa v DB: '.$notInDB);
+            $output->writeln('Mame v exporte a nasli sa v DB: '.$inDB);
+            $output->writeln('Pocet updatov: '.count($updates));
+            $output->writeln('Updatli sa v DB: '.$updated);
+
         } catch (Exception $e) {
             $conn->rollback();
             throw $e;
