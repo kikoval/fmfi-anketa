c4887b41efd5d97191ae2e409f11cf42d933bd7e
Kristian Valentin <valentin.kristian@gmail.com>
2013-05-09 18:49:28 +0200

    pridana funkcia na poslanie emailu, pri priradovani ucitela k predmetu sa pridava novy zaznam do tabulky UserSeason

--- src/AnketaBundle/Controller/TeachingAssociationAdminController.php
+++ src/AnketaBundle/Controller/TeachingAssociationAdminController.php
@@ -2,6 +2,8 @@
 
 namespace AnketaBundle\Controller;
 
+use AnketaBundle\Entity\UserSeason;
+
 use Doctrine\DBAL\DBALException;
 
 use AnketaBundle\Entity\TeachersSubjects;
@@ -21,6 +23,7 @@ class TeachingAssociationAdminController extends Controller {
     }
 
     // TODO pagination
+    // TODO group by teacher id, zobrazit pocet rovnakych cez COUNT()
     public function indexAction() {
         $em = $this->getDoctrine()->getManager();
 
@@ -36,6 +39,7 @@ class TeachingAssociationAdminController extends Controller {
                         array('tas' => $tas));
     }
 
+    // TODO send email to the user that reported the issue (and display a message about it)
     public function processRequestAction() {
         $approve = $this->getRequest()->get('approve', null);
         if ($approve !== null) {
@@ -46,6 +50,28 @@ class TeachingAssociationAdminController extends Controller {
         if ($mark_as_completed !== null) {
             return $this->markAsCompleted();
         }
+    }
+    
+    public function sendEmailAfterApproval(User $user) {
+        
+        $to = $user->getLogin().'@uniba.sk';
+        $sender = $this->container->getParameter('mail_sender');
+        $body = $this->renderView('AnketaBundle:TeachingAssociationAdmin:email.txt.twig');
+        $skratkaFakulty = $this->container->getParameter('skratka_fakulty');
+        
+        $this->get('mailer'); // DO NOT DELETE THIS LINE
+        // it autoloads required things before Swift_Message can be used
+        
+        $message = \Swift_Message::newInstance()
+        ->setSubject($skratkaFakulty . ' ANKETA -- requested teacher')
+        ->setFrom($sender)
+        ->setTo($to)
+        ->setBody($body);
+        $this->get('mailer')->send($message);
+        
+        $this->get('session')->getFlashBag()->add('success',
+                'Bol poslaný email o úspešnom vyriešení požiadavky používateľovi
+                , ktorý ju nahlásil.');
     }
     
     public function addTeacherToSubject() {
@@ -60,6 +86,18 @@ class TeachingAssociationAdminController extends Controller {
 
         if ($ta !== null && $ta->getTeacher() !== null
                 && $ta->getSubject() !== null && $ta->getSeason() !== null) {
+            
+            // create and store UserSeason
+            $userSeason = new UserSeason();
+            $userSeason->setIsStudent(false);
+            $userSeason->setIsTeacher(true);
+            $userSeason->setSeason($ta->getSeason());
+            $userSeason->setUser($ta->getTeacher());
+            
+            $em->persist($userSeason);
+            $em->flush();
+            
+            // create and store TeachersSubjects
             $teachersSubjects = new TeachersSubjects($ta->getTeacher(),
                     $ta->getSubject(), $ta->getSeason());
 
@@ -73,7 +111,7 @@ class TeachingAssociationAdminController extends Controller {
                 $em->flush();
             } catch (DBALException $e) {
                 // TODO check $e says entry is duplicated
-                $session->getFlashBag()->add('warning', 'Učiteľ už je priradený k predmetu.');
+                $session->getFlashBag()->add('error', 'Učiteľ už je priradený k predmetu.');
                 return $this->redirect($this->generateUrl(
                         'admin_teaching_associations'));
             }
@@ -82,6 +120,9 @@ class TeachingAssociationAdminController extends Controller {
             $ta->setCompleted(true);
             $em->persist($ta);
             $em->flush();
+            
+            if ($ta->getRequestedBy() !== null)
+                $this->sendEmailAfterApproval($ta->getRequestedBy());
             
             $session->getFlashBag()->add('succcess', 'Učiteľ bol úspešne priradený k predmetu.');
             
@@ -103,6 +144,9 @@ class TeachingAssociationAdminController extends Controller {
             $ta->setCompleted(true);
             $em->persist($ta);
             $em->flush();
+            
+            if ($ta->getRequestedBy() !== null)
+                $this->sendEmailAfterApproval($ta->getRequestedBy());
         }
         
         return $this->redirect($this->generateUrl(
