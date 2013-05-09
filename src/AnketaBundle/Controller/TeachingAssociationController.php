294fef4d314a926eb5d1dc9c2248ebc05bee6630
Kristian Valentin <valentin.kristian@gmail.com>
2013-05-09 19:12:08 +0200

    vytvaranie usera ak nie je v DB, ale je v LDAP

--- src/AnketaBundle/Controller/TeachingAssociationController.php
+++ src/AnketaBundle/Controller/TeachingAssociationController.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @copyright Copyright (c) 2013 The FMFI Anketa authors (see AUTHORS).
+ * Use of this source code is governed by a license that can be
+ * found in the LICENSE file in the project root directory.
+ *
+ * @package    Anketa
+ * @subpackage Controller
+ *
+ */
 
 namespace AnketaBundle\Controller;
 
@@ -19,8 +28,7 @@ class TeachingAssociationController extends Controller
         	throw new AccessDeniedException();
     }
 
-    public function formAction($subject_slug)
-    {
+    public function formAction($subject_slug) {
         $em = $this->get('doctrine.orm.entity_manager');
         $repo = $em->getRepository('AnketaBundle\Entity\Subject');
         $subject = $repo->findOneBy(array('slug' => $subject_slug));
@@ -32,8 +40,7 @@ class TeachingAssociationController extends Controller
                 array('subject'=>$subject));
     }
 
-    public function processFormAction($subject_slug)
-    {
+    public function processFormAction($subject_slug) {
         $request = $this->get('request');
         $em = $this->get('doctrine.orm.entity_manager');
         $subjectRepository = $em->getRepository('AnketaBundle\Entity\Subject');
@@ -53,19 +60,34 @@ class TeachingAssociationController extends Controller
         $is_trainer = $request->request->get('teacher-assistant', false);
         $teacher_login = $request->get('teacher-login', null);
         $teacher_name = $request->get('teacher-name', null);
-        
+        $teacher_givenName = $request->get('teacher-given-name', null);
+        $teacher_familyName = $request->get('teacher-family-name', null);
+
         $teacher_info = sprintf('Meno: %s, login: %s', $teacher_name, $teacher_login);
 
         // if the teacher login is provided, use it to find the teacher
         $teacher = null;
-        if ($teacher_login !== null)
+        if ($teacher_login !== null) {
             $teacher = $userRepository->findOneBy(array('login' => $teacher_login));
+        }
 
+        // add a user when he is not in DB, but he is in LDAP
+        if ($teacher == null && $teacher_login !== null) {
+            $teacher = new User($teacher_login);
+            $teacher->setDisplayName($teacher_name);
+            $teacher->setGivenName($teacher_givenName);
+            $teacher->setFamilyName($teacher_familyName);
+            
+            $em->persist($teacher);
+            $em->flush();
+        }
+        
         $assoc = new TeachingAssociation($season, $subject, $teacher, $user,
                 $note.PHP_EOL.$teacher_info, $is_lecturer, $is_trainer);
         $em->persist($assoc);
         $em->flush();
 
+        // send email about the request
         $emailTpl = array(
                 'subject' => $subject,
                 'teacher' => $teacher_info,
@@ -78,8 +100,8 @@ class TeachingAssociationController extends Controller
         $body = $this->renderView('AnketaBundle:TeachingAssociation:email.txt.twig', $emailTpl);
         $skratkaFakulty = $this->container->getParameter('skratka_fakulty');
 
-       $this->get('mailer'); // DO NOT DELETE THIS LINE
-       // it autoloads required things before Swift_Message can be used
+        $this->get('mailer'); // DO NOT DELETE THIS LINE
+        // it autoloads required things before Swift_Message can be used
 
         $message = \Swift_Message::newInstance()
                 ->setSubject($skratkaFakulty . ' ANKETA -- requested teacher')
@@ -88,11 +110,12 @@ class TeachingAssociationController extends Controller
                 ->setBody($body);
         $this->get('mailer')->send($message);
 
-        $session = $this->get('session');
-        $session->setFlash('success',
+        // display message
+        $uni_email = $user->getLogin().'@uniba.sk';
+        $this->get('session')->getFlashBag()->add('success',
                 'Ďakujeme za informáciu. ' .
-                'V priebehu pár dní by mala byť spracovaná, preto si nezabudnite ' .
-                'otvoriť anketu znovu a ohodnotiť prípadných pridaných učiteľov.');
+                'V priebehu pár dní by mala byť spracovaná. Následne Vás budeme
+                informovať správou na Váš univerzitný email ('.$uni_email.').');
 
         return new RedirectResponse($this->generateUrl(
                 'answer_subject', array('subject_slug'=>$subject->getSlug())));
