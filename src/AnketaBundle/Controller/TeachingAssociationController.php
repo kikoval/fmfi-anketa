65200b89ce9709fcf15b106a9de7a510a5dd63aa
Kristian Valentin <valentin.kristian@gmail.com>
2013-05-10 11:15:25 +0200

    teacher_info v note uz nie je treba

--- src/AnketaBundle/Controller/TeachingAssociationController.php
+++ src/AnketaBundle/Controller/TeachingAssociationController.php
@@ -64,8 +64,6 @@ class TeachingAssociationController extends Controller
         $teacher_givenName = $request->get('teacher-given-name', null);
         $teacher_familyName = $request->get('teacher-family-name', null);
 
-        $teacher_info = sprintf('Meno: %s, login: %s', $teacher_name, $teacher_login);
-
         // if the teacher login is provided, use it to find the teacher
         $teacher = null;
         if ($teacher_login !== null) {
@@ -73,7 +71,7 @@ class TeachingAssociationController extends Controller
         }
 
         // add a user when he is not in DB, but he is in LDAP
-        if ($teacher == null && $teacher_login !== null) {
+        if ($teacher === null && $teacher_login !== null) {
             $teacher = new User($teacher_login);
             $teacher->setDisplayName($teacher_name);
             $teacher->setGivenName($teacher_givenName);
@@ -83,15 +81,16 @@ class TeachingAssociationController extends Controller
             $em->flush();
         }
 
+        // create "ticket" for the association
         $assoc = new TeachingAssociation($season, $subject, $teacher, $user,
-                $note.PHP_EOL.$teacher_info, $is_lecturer, $is_trainer);
+                $note, $is_lecturer, $is_trainer);
         $em->persist($assoc);
         $em->flush();
 
         // send email about the request
         $emailTpl = array(
                 'subject' => $subject,
-                'teacher' => $teacher_info,
+                'teacher' => $teacher,
                 'is_lecturer' => $is_lecturer,
                 'is_trainer' => $is_trainer,
                 'note' => $note,
@@ -112,6 +111,7 @@ class TeachingAssociationController extends Controller
         $this->get('mailer')->send($message);
 
         // display message
+        // email will be send after completing the request in admin section
         $uni_email = $user->getLogin().'@uniba.sk';
         $this->get('session')->getFlashBag()->add('success',
                 'Ďakujeme za informáciu. ' .
